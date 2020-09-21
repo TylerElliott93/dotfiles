af137a1356ffbbee8f247ef8429afbd5da1f49bd
Tyler Elliott <tyler.elliott.1993@gmail.com>
2020-09-21 22:12:41 +1000

    Added apps to install script

---
+++ install-apps.sh
@@ -0,0 +1,12 @@
+echo "Installing Apps"
+
+brew cask install anki
+brew cask install ccleaner
+brew cask install discord
+brew cask install firefox
+brew cask install slack
+brew cask install sublime-text
+brew cask install xtorrent
+brew cask install vlc
+brew cask install whatsapp
+brew cask install google-chrome
